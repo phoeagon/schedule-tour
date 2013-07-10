16bb32270c49c091eb758ff5ce70a6c0576d4053
phoeagon <phoeagon@gmail.com>
2013-07-10 16:05:46 +0800

    changed mongoose structure

--- code/prototype/site/models/types/eventEntry.js
+++ code/prototype/site/models/types/eventEntry.js
@@ -1,9 +1,4 @@
-var mongodb = require('../DB');
-var mongoose = require('mongoose');
-
-var DBNAME = 'scheduletour'
-var TABLENAME = 'eventEntry'
-mongoose.connect('mongodb://localhost/'+DBNAME);
+var mongoose = require('../mongoose');
 
 var eventEntryPrototype = {
     name : String ,
@@ -14,9 +9,13 @@ function eventEntry( eventEntry ) {
     for ( var ele in eventEntry )
         this[ele] = eventEntry[ele];
 };
+var TABLENAME = 'eventEntry'
 module.exports = eventEntry;
 
 var eventEntry = mongoose.model( TABLENAME , eventEntryPrototype );
 
 //var ee = new eventEntry({name:'test',place:'SE',time:new Date()});
 //ee.save();
+eventEntry.find({},function(err,obj){
+    console.log( obj );
+})
