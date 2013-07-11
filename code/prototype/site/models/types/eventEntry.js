c878c75464c52e07e23d5b0e86bac9c75e56c858
MinGKai <MinGKaiDong@gmail.com>
2013-07-11 15:19:30 +0800

    add: schema of user
    Use mongoose as ORM-framework.

--- code/prototype/site/models/types/eventEntry.js
+++ code/prototype/site/models/types/eventEntry.js
@@ -1,21 +1,25 @@
 var mongoose = require('../mongoose');
 
-var eventEntryPrototype = {
-    name : String ,
-    place : String ,
-    time : Date 
-};
-function eventEntry( eventEntry ) {
-    for ( var ele in eventEntry )
-        this[ele] = eventEntry[ele];
-};
-var TABLENAME = 'eventEntry'
-module.exports = eventEntry;
+//schema of EventEntry
+var EventEntrySchema = mongoose.Schema({
+    name    :   String ,
+    place   :   String ,
+    time    :   Date 
+});
 
-var eventEntry = mongoose.model( TABLENAME , eventEntryPrototype );
+var TABLENAME = 'EventEntry';
+//compile into a model
+var EventEntry = mongoose.model( TABLENAME , EventEntrySchema );
 
-//var ee = new eventEntry({name:'test',place:'SE',time:new Date()});
-//ee.save();
-eventEntry.find({},function(err,obj){
-    console.log( obj );
-})
+
+//function to test eventEntry
+var testEventEntry = function() {
+    var ee = new EventEntry({name:'test',place:'SE',time:new Date()});
+    ee.save();
+    EventEntry.find({}, function(err, obj) {
+        console.log( obj );
+    });
+}
+
+//export module
+module.exports = EventEntry;
