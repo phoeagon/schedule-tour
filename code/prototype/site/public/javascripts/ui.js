43bdd685946ca74f3cf5492da6250604e29f6c56
phoeagon <phoeagon@gmail.com>
2013-07-12 09:25:43 +0800

    added basic dialog

---
+++ code/prototype/site/public/javascripts/ui.js
@@ -0,0 +1,23 @@
+$( "#add_event_gesture_dialog" ).dialog({
+    autoOpen: true,
+    width: 400,
+    buttons: [
+        {
+            text: "Ok",
+            click: function() {
+                $( this ).dialog( "close" );
+            }
+        },
+        {
+            text: "Cancel",
+            click: function() {
+                $( this ).dialog( "close" );
+            }
+        }
+    ]
+});
+$( "#add_event_gesture_dialog" ).dialog( "open" );
+$(function() {
+    $( ".datepicker" ).datepicker();
+    $( ".slider" ).slider();
+});
