7d35116892b1a5a18d09a57e1ca2ad49ed2d7a49
phoeagon <phoeagon@gmail.com>
2013-07-17 15:57:32 +0800

    moved calendar renderer

---
+++ code/prototype/site/public/javascripts/calendar.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	    $.get('html.calender',function(data){
+		//$('#calendar').html(data);
+		if (globalEventCache)
+		    var events = globalEventCache.eventEntries;
+		else
+		    var events = [];
+		var feed = [];
+		for ( var e in events ){
+		    feed.push( { title: events[e].title ,
+				 start: new Date(events[e].time) ,
+				 end: moment(events[e].time).add('hour',1).toDate() ,
+				 //tmp, assume an hour
+				 allDay: false
+				 // assume not allday
+			     })
+		}
+		console.log( "feed=");
+		console.log( feed );
+		$('#calendar').fullCalendar({
+			    header: {
+				    left: 'prev,next today',
+				    center: 'title',
+				    right: 'month,agendaWeek,agendaDay'
+			    },
+			    events : feed , 
+			    eventClick: function(event) {
+				    // opens events in a popup window
+				    window.open(event.url, 'gcalevent', 'width=700,height=600');
+				    return false;
+			    },
+			    
+			    loading: function(bool) {
+				    if (bool) {
+					    $('#loading').show();
+				    }else{
+					    $('#loading').hide();
+				    }
+			    }
+			});
+	    });
+})
