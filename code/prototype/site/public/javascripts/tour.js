5c9685389aa093838f83ab555d8afae7150b0e77
mfy <mfy0120@gmail.com>
2013-07-19 16:37:24 +0800

    Recovery from crash

--- code/prototype/site/public/javascripts/tour.js
+++ code/prototype/site/public/javascripts/tour.js
@@ -105,13 +105,14 @@ var checkFeasibility = function(route) {
 					route[i] = route[i1];
 					route[i1] = tmp;
 				}
-		}
+	}
     //check the time spent between adjoining events
     for (var i=0; i<route.length-1; ++i) {
         if (eventEntries[route[i]].time+ eventEntries[route[i]].duration + getPathTime(route[i], route[i+1]) > eventEntries[route[i+1]].time) {
             if (eventEntries[route[i]].weight == eventEntries[route[i+1]].weight) {
                 //throw the warning
-                throw Exception("You needs to change your schedule", route[i], route[i+1]);
+             //   throw Exception("You needs to change your schedule", route[i], route[i+1]);
+			 	route.splice(i+1 ,1);
                 return;
             }
             //remove the event with less weight
