22d2bd8a097c0fd362d011b2221a66a69d6a26e6
phoeagon <phoeagon@gmail.com>
2013-07-30 15:50:29 +0800

    fix UTC local time lag on timeline

--- code/prototype/site/public/javascripts/timeline.js
+++ code/prototype/site/public/javascripts/timeline.js
@@ -1,10 +1,12 @@
 $.getJSON('/calendarentries' , function(data){
             console.log( JSON.stringify(data) )
             for (var ele in data ){
+                var t1 = moment( data[ele].start );
+                var t2 = moment( data[ele].end );
                 var content_tag =
-                    "<b>"+data[ele].title+"</b><br/>Start: "+data[ele].start;
-                data[ele].start = new Date( data[ele].start )
-                data[ele].end = new Date( data[ele].end )
+                    "<b>"+data[ele].title+"</b><br/>Start: "+t1.calendar();
+                data[ele].start = t1.toDate()
+                data[ele].end = t2.toDate()
                 if ( data[ele].end.valueOf() - data[ele].start.valueOf() < 1000*3600 )
                     delete data[ele].end;
                 data[ele].content = content_tag ;
