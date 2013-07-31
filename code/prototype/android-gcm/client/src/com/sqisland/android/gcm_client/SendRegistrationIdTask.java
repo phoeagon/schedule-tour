2253c7fedc98788ea91caa1a91dee8b072cd58cc
phoeagon <phoeagon@gmail.com>
2013-07-31 16:22:26 +0800

    GCM client change layout

--- code/prototype/android-gcm/client/src/com/sqisland/android/gcm_client/SendRegistrationIdTask.java
+++ code/prototype/android-gcm/client/src/com/sqisland/android/gcm_client/SendRegistrationIdTask.java
@@ -14,6 +14,8 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.TextView;
@@ -22,13 +24,18 @@ final class SendRegistrationIdTask extends
 AsyncTask<String, Void, HttpResponse> {
 	private String mRegId;
 	private SendMsgStatus parent;
+	SharedPreferences prefs ;
 	
 	public SendRegistrationIdTask(String regId) {
 		mRegId = regId;
+		prefs = parent.getSharedPreferences(
+				Constants.APP, Context.MODE_PRIVATE);
 	}
 	public SendRegistrationIdTask(String regId,SendMsgStatus parent) {
 		mRegId = regId;
 		this.parent = parent;
+		prefs = parent.getSharedPreferences(
+			      Constants.APP, Context.MODE_PRIVATE);
 	}
 	public void setParent( SendMsgStatus parent ){
 		this.parent = parent;
@@ -37,10 +44,18 @@ AsyncTask<String, Void, HttpResponse> {
 	protected HttpResponse doInBackground(String... regIds) {
 		String url = Constants.SERVER_URL + "/gcmRegistry";
 		HttpPost httppost = new HttpPost(url);
-		
+		final String username = prefs.getString("username", "new");
+		Log.d("Prefs",username);
+		final String password = prefs.getString("password", "");
+		Log.d("Prefs",password);
+		final Boolean notification = 
+				prefs.getBoolean("notif", true);
 		try {
 		  List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
 		  nameValuePairs.add(new BasicNameValuePair("deviceID", mRegId));
+		  nameValuePairs.add(new BasicNameValuePair("username", username));
+		  nameValuePairs.add(new BasicNameValuePair("password", password));
+		  nameValuePairs.add(new BasicNameValuePair("notif", ""+notification));
 		  httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 		  HttpClient httpclient = new DefaultHttpClient();
 		  return httpclient.execute(httppost);
@@ -55,6 +70,8 @@ AsyncTask<String, Void, HttpResponse> {
 	
 	@Override
 	protected void onPostExecute(HttpResponse response) {
+		final int status_code = response.getStatusLine().getStatusCode();
+		Log.d("execute",""+status_code);
 		if (response == null) {
 		  Log.e(Constants.TAG, "HttpResponse is null");
 		  return;
