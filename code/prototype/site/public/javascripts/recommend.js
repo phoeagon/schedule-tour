eabab3fc5f219da1367e44b22cb8d5e6787934ab
MinGKai <MinGKaiDong@gmail.com>
2013-08-02 11:35:57 +0800

    fix: missing of douban_recomendation

--- code/prototype/site/public/javascripts/recommend.js
+++ code/prototype/site/public/javascripts/recommend.js
@@ -15,9 +15,13 @@ var recommend_douban = function(map) {
             var events = res.events;
             events.map(function(e) {
                 var point = e.geo.split(' ');
-                var point = new ScheduleTour.Point(point[1], point[0]);
-                if (point.lng === point.lat && point.lng === 0) return;
-                var marker = new ScheduleTour.Marker(point);
+                var point = new ScheduleTour.Point(point[0], point[1]);
+                if (point.lng() === point.lat() && point.lng() === 0) return;
+                var marker = new ScheduleTour.Marker({
+                    map         :   map,
+                    position    :   point,
+                    title   :   e.title
+                });
 
                 var opts = {
                     title : '<b>'+e.title+'</b>'
@@ -26,16 +30,15 @@ var recommend_douban = function(map) {
                     "<p style='margin:0;line-height:1.5;font-size:13px;text-indent:2em'>" + e.content.substring(0,300) + "...</p>" + 
                     "<p>See more <a href='" + e.alt + "' target='_blank'>here</a> on Douban</p>" +
                     "</div>";
-                var infoWindow = new ScheduleTour.InfoWindow(sContent, opts);  // 创建信息窗口对象
-                marker.setMap(map);
+                var infoWindow = new ScheduleTour.InfoWindow({
+                    content:    sContent
+                });
+                //marker.setMap(map);
                 //DEPRECATED:map.addOverlay(marker);
                 //DEPRECATED: marker.addEventListener("click", function(){
-                marker.addListener("click", function(){       
-                    this.openInfoWindow(infoWindow);
-                    //图片加载完毕重绘infowindow
-                    document.getElementById('imgDemo').onload = function (){
-                        infoWindow.redraw();   //防止在网速较慢，图片未加载时，生成的信息框高度比图片的总高度小，导致图片部分被隐藏
-                    }
+                google.maps.event.addListener(marker, 'click', function() {
+                    infoWindow.open(map, marker);
+                //    placeManager.configureButton( map )
                 });
             });
         },
@@ -60,9 +63,13 @@ recommend_douban_db = function(map, lng, lat, dist, num) {
             var events = res.events;
             events.map(function(e) {
                 var point = e.geo.split(' ');
-                var point = new ScheduleTour.Point(point[1], point[0]);
-                if (point.lng === point.lat && point.lng === 0) return;
-                var marker = new ScheduleTour.Marker(point);
+                var point = new ScheduleTour.Point(point[0], point[1]);
+                if (point.lng() === point.lat() && point.lng() === 0) return;
+                var marker = new ScheduleTour.Marker({
+                    map         :   map,
+                    position    :   point,
+                    title   :   e.title
+                });
 
                 var opts = {
                     title : '<b>'+e.title+'</b>'
@@ -71,16 +78,15 @@ recommend_douban_db = function(map, lng, lat, dist, num) {
                     "<p style='margin:0;line-height:1.5;font-size:13px;text-indent:2em'>" + e.content.substring(0,300) + "...</p>" + 
                     "<p>See more <a href='" + e.alt + "' target='_blank'>here</a> on Douban</p>" +
                     "</div>";
-                var infoWindow = new ScheduleTour.InfoWindow(sContent, opts);  // 创建信息窗口对象
-                marker.setMap(map);
+                var infoWindow = new ScheduleTour.InfoWindow({
+                    content:sContent
+                    });  // 创建信息窗口对象
+                //marker.setMap(map);
                 //DEPRECATED map.addOverlay(marker);
                 //DEPRECATED: marker.addEventListener("click", function(){
-                marker.addListener("click", function(){
-                    this.openInfoWindow(infoWindow);
-                    //图片加载完毕重绘infowindow
-                    document.getElementById('imgDemo').onload = function (){
-                        infoWindow.redraw();   //防止在网速较慢，图片未加载时，生成的信息框高度比图片的总高度小，导致图片部分被隐藏
-                    }
+                google.maps.event.addListener(marker, 'click', function() {
+                    infoWindow.open(map, marker);
+                //    placeManager.configureButton( map )
                 });
             });
         }
