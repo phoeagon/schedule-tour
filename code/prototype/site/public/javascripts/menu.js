675f69f4eae64a7fa5b45fcfcdec0403215fec55
phoeagon <phoeagon@gmail.com>
2013-08-02 13:39:22 +0800

    draggable avatar

--- code/prototype/site/public/javascripts/menu.js
+++ code/prototype/site/public/javascripts/menu.js
@@ -1,6 +1,8 @@
 $(document).ready( function(){
-    $('#avatar_div').click ( function(){
-        $('#menu_div').removeClass('displaynone');
+    $('#avatar_div').click ( function(e){
+        console.log ( e )
+        $('#menu_div').css('position','absolute')
+                .css('top',e.clientY).css('left',e.clientX).removeClass('displaynone');
     } )
     $('#menu').mouseleave( function(){
         $('#menu_div').addClass('displaynone');
